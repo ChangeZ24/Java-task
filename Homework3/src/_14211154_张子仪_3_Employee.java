5a312f23fd0e3ba9711f389128d5fc0982ec2223
ChangeLynn <bian.love@qq.com>
2017-03-07 19:52:10 +0800

    Add files via upload

---
+++ Homework3/src/_14211154_张子仪_3_Employee.java
@@ -0,0 +1,30 @@
+package homework3;
+
+public abstract class _14211154_张子仪_3_Employee {
+	String firstName;
+	String lastName;
+	String socialSecurityNumber;
+	public String getFirstName() {
+		return this.firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return this. lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSocialSecurityNumber() {
+		return this.socialSecurityNumber;
+	}
+	public _14211154_张子仪_3_Employee(String firstName,String lastName,String socialSecurityNumber){
+		this.setFirstName(firstName);
+		this.firstName=this.getFirstName();
+		this.setLastName(lastName);
+		this.lastName=this.getLastName();
+		this.socialSecurityNumber=socialSecurityNumber;
+	}
+	public abstract double earning();
+}
