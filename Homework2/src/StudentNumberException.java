308b3f854b8d8c44e2f708b185242d48fc905892
ChangeLynn <bian.love@qq.com>
2017-03-07 19:48:20 +0800

    Delete StudentNumberException.java

--- Homework2/src/StudentNumberException.java
+++
@@ -1,7 +0,0 @@
-package homework2;
-import java.lang.Exception;
-public class StudentNumberException extends Exception {
-    public StudentNumberException(String message) {
-    	super(message);
-    }
-}
